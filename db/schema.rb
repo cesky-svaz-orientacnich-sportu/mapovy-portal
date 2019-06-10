acea319f4917c7bfdcabede79a532e36cc9b2381
David Pustai <david@pustai.cz>
2019-06-10 16:49:40 +0200

    Update `schema.rb`

--- db/schema.rb
+++ db/schema.rb
@@ -11,207 +11,208 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150409051302) do
+ActiveRecord::Schema.define(version: 20150613152413) do
 
-  create_table "authorizations", force: true do |t|
-    t.string   "provider"
-    t.string   "uid"
-    t.integer  "user_id"
-    t.string   "token"
-    t.string   "secret"
-    t.string   "name"
-    t.string   "link"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "authorizations", force: :cascade do |t|
+    t.string   "provider",   limit: 255
+    t.string   "uid",        limit: 255
+    t.integer  "user_id",    limit: 4
+    t.string   "token",      limit: 255
+    t.string   "secret",     limit: 255
+    t.string   "name",       limit: 255
+    t.string   "link",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
-  create_table "authors", force: true do |t|
-    t.string   "note"
-    t.string   "full_name"
-    t.text     "club"
-    t.string   "activity"
-    t.integer  "year_of_birth"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+  create_table "authors", force: :cascade do |t|
+    t.string   "note",          limit: 255
+    t.string   "full_name",     limit: 255
+    t.text     "club",          limit: 65535
+    t.string   "activity",      limit: 255
+    t.integer  "year_of_birth", limit: 4
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
-  create_table "cartographers", force: true do |t|
-    t.integer  "map_id"
-    t.integer  "author_id"
-    t.string   "role"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "cartographers", force: :cascade do |t|
+    t.integer  "map_id",     limit: 4
+    t.integer  "author_id",  limit: 4
+    t.string   "role",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
-  create_table "clubs", force: true do |t|
-    t.string   "name"
-    t.string   "abbreviation"
-    t.string   "region"
-    t.string   "district"
-    t.string   "url"
-    t.string   "division"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.text     "oris_data_json"
-    t.string   "slug"
+  create_table "clubs", force: :cascade do |t|
+    t.string   "name",           limit: 255
+    t.string   "abbreviation",   limit: 255
+    t.string   "region",         limit: 255
+    t.string   "district",       limit: 255
+    t.string   "url",            limit: 255
+    t.string   "division",       limit: 255
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.text     "oris_data_json", limit: 65535
+    t.string   "slug",           limit: 255
   end
 
   add_index "clubs", ["slug"], name: "index_clubs_on_slug", unique: true, using: :btree
 
-  create_table "issue_reports", force: true do |t|
-    t.integer  "map_id"
-    t.integer  "user_id"
-    t.string   "email"
-    t.text     "message"
+  create_table "issue_reports", force: :cascade do |t|
+    t.integer  "map_id",     limit: 4
+    t.integer  "user_id",    limit: 4
+    t.string   "email",      limit: 255
+    t.text     "message",    limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "resolved",   default: false, null: false
+    t.boolean  "resolved",                 default: false, null: false
   end
 
-  create_table "map_collection_memberships", force: true do |t|
-    t.integer  "map_id"
-    t.integer  "map_collection_id"
-    t.integer  "year"
-    t.string   "note"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+  create_table "map_collection_memberships", force: :cascade do |t|
+    t.integer  "map_id",            limit: 4
+    t.integer  "map_collection_id", limit: 4
+    t.integer  "year",              limit: 4
+    t.string   "note",              limit: 255
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
   end
 
-  create_table "map_collections", force: true do |t|
-    t.string   "title"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.string   "slug"
-    t.text     "description"
-    t.string   "logo_file_name"
-    t.string   "logo_content_type"
-    t.integer  "logo_file_size"
+  create_table "map_collections", force: :cascade do |t|
+    t.string   "title",             limit: 255
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "slug",              limit: 255
+    t.text     "description",       limit: 65535
+    t.string   "logo_file_name",    limit: 255
+    t.string   "logo_content_type", limit: 255
+    t.integer  "logo_file_size",    limit: 4
     t.datetime "logo_updated_at"
   end
 
   add_index "map_collections", ["slug"], name: "index_map_collections_on_slug", unique: true, using: :btree
 
-  create_table "map_layers", force: true do |t|
-    t.integer  "layer_index"
-    t.string   "title_cs"
-    t.string   "title_en"
-    t.string   "color"
+  create_table "map_layers", force: :cascade do |t|
+    t.integer  "layer_index",        limit: 4
+    t.string   "title_cs",           limit: 255
+    t.string   "title_en",           limit: 255
+    t.string   "color",              limit: 255
     t.boolean  "visible_by_default"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
   end
 
-  create_table "maps", force: true do |t|
-    t.string   "title"
-    t.string   "patron"
-    t.string   "patron_accuracy"
-    t.integer  "year"
-    t.string   "year_accuracy"
-    t.integer  "scale"
-    t.string   "archive_print1_class"
-    t.string   "archive_print2_class"
-    t.string   "archive_print3_class"
-    t.integer  "archive_extra_print_count"
-    t.float    "equidistance"
-    t.string   "identifier_other"
-    t.string   "locality"
-    t.float    "area_size"
-    t.string   "issued_by"
-    t.string   "printed_by"
-    t.string   "map_type"
-    t.string   "drawing_technique"
-    t.string   "printing_technique"
-    t.string   "resource"
-    t.string   "main_race_title"
+  create_table "maps", force: :cascade do |t|
+    t.string   "title",                     limit: 255
+    t.string   "patron",                    limit: 255
+    t.string   "patron_accuracy",           limit: 255
+    t.integer  "year",                      limit: 4
+    t.string   "year_accuracy",             limit: 255
+    t.integer  "scale",                     limit: 4
+    t.string   "archive_print1_class",      limit: 255
+    t.string   "archive_print2_class",      limit: 255
+    t.string   "archive_print3_class",      limit: 255
+    t.integer  "archive_extra_print_count", limit: 4
+    t.float    "equidistance",              limit: 24
+    t.string   "identifier_other",          limit: 255
+    t.string   "locality",                  limit: 255
+    t.float    "area_size",                 limit: 24
+    t.string   "issued_by",                 limit: 255
+    t.string   "printed_by",                limit: 255
+    t.string   "map_type",                  limit: 255
+    t.string   "drawing_technique",         limit: 255
+    t.string   "printing_technique",        limit: 255
+    t.string   "resource",                  limit: 255
+    t.string   "main_race_title",           limit: 255
     t.date     "main_race_date"
-    t.text     "administrator"
-    t.string   "identifier_approval"
-    t.string   "identifier_filing"
-    t.text     "note_public"
-    t.text     "note_internal"
-    t.string   "preview_identifier"
-    t.datetime "created_at",                                null: false
-    t.datetime "updated_at",                                null: false
-    t.integer  "created_by_id"
-    t.string   "map_family"
-    t.string   "map_sport"
-    t.integer  "oris_event_id"
-    t.text     "shape_json"
-    t.text     "shape_kml"
-    t.string   "georeference"
-    t.string   "region"
-    t.string   "state"
-    t.text     "record_log"
-    t.integer  "approved_by_id"
-    t.string   "mapping_state"
-    t.string   "slug"
-    t.boolean  "has_jpg",                   default: false, null: false
-    t.boolean  "has_kml",                   default: false, null: false
-    t.string   "administrator_email"
+    t.text     "administrator",             limit: 65535
+    t.string   "identifier_approval",       limit: 255
+    t.string   "identifier_filing",         limit: 255
+    t.text     "note_public",               limit: 65535
+    t.text     "note_internal",             limit: 65535
+    t.string   "preview_identifier",        limit: 255
+    t.datetime "created_at",                                              null: false
+    t.datetime "updated_at",                                              null: false
+    t.integer  "created_by_id",             limit: 4
+    t.string   "map_family",                limit: 255
+    t.string   "map_sport",                 limit: 255
+    t.integer  "oris_event_id",             limit: 4
+    t.text     "shape_json",                limit: 65535
+    t.text     "shape_kml",                 limit: 65535
+    t.string   "georeference",              limit: 255
+    t.string   "region",                    limit: 255
+    t.string   "state",                     limit: 255
+    t.text     "record_log",                limit: 65535
+    t.integer  "approved_by_id",            limit: 4
+    t.string   "mapping_state",             limit: 255
+    t.string   "slug",                      limit: 255
+    t.boolean  "has_jpg",                                 default: false, null: false
+    t.boolean  "has_kml",                                 default: false, null: false
+    t.string   "administrator_email",       limit: 255
     t.date     "last_reminder_sent_at"
     t.date     "state_changed_at"
-    t.integer  "completed_by_id"
+    t.integer  "completed_by_id",           limit: 4
+    t.datetime "user_updated_at"
   end
 
   add_index "maps", ["slug"], name: "index_maps_on_slug", unique: true, using: :btree
 
-  create_table "oris_events", force: true do |t|
-    t.integer  "oris_id"
+  create_table "oris_events", force: :cascade do |t|
+    t.integer  "oris_id",           limit: 4
     t.date     "date"
-    t.string   "title"
-    t.text     "oris_json",         limit: 2147483647
-    t.string   "place"
+    t.string   "title",             limit: 255
+    t.text     "oris_json",         limit: 4294967295
+    t.string   "place",             limit: 255
     t.datetime "oris_timestamp"
     t.datetime "created_at",                           null: false
     t.datetime "updated_at",                           null: false
-    t.string   "obpostupy_url"
-    t.string   "obpostupy_map_url"
+    t.string   "obpostupy_url",     limit: 255
+    t.string   "obpostupy_map_url", limit: 255
   end
 
-  create_table "texts", force: true do |t|
-    t.string   "name"
-    t.text     "body_cs"
-    t.text     "body_en"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "texts", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.text     "body_cs",    limit: 65535
+    t.text     "body_en",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "confirmation_token"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "confirmation_token",     limit: 255
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.string   "oris_registrations"
-    t.string   "role"
-    t.string   "authorized_clubs"
-    t.string   "authorized_regions"
-    t.string   "full_name"
+    t.string   "unconfirmed_email",      limit: 255
+    t.string   "oris_registrations",     limit: 255
+    t.string   "role",                   limit: 255
+    t.string   "authorized_clubs",       limit: 255
+    t.string   "authorized_regions",     limit: 255
+    t.string   "full_name",              limit: 255
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "versions", force: true do |t|
-    t.string   "item_type",      null: false
-    t.integer  "item_id",        null: false
-    t.string   "event",          null: false
-    t.string   "whodunnit"
-    t.text     "object"
+  create_table "versions", force: :cascade do |t|
+    t.string   "item_type",      limit: 255,   null: false
+    t.integer  "item_id",        limit: 4,     null: false
+    t.string   "event",          limit: 255,   null: false
+    t.string   "whodunnit",      limit: 255
+    t.text     "object",         limit: 65535
     t.datetime "created_at"
-    t.text     "object_changes"
+    t.text     "object_changes", limit: 65535
   end
 
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
