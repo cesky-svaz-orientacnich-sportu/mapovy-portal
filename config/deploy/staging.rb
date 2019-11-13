4108aeaf9d4efc1098d8fae7e83b4df63e651d31
David Pustai <david@pustai.cz>
2019-11-13 14:32:55 +0100

    Set up new staging env

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 # -*- encoding : utf-8 -*-
-set :deploy_to, '/home/mapserver/mapserver.staging'
-server 'siven.onesim.net', :app, :web, :db, :primary => true
+set :deploy_to, '/home/mapserver/mapserver_staging'
+server 'goldfish.amagical.net', :app, :web, :db, :primary => true
 set :rails_env, 'staging'
