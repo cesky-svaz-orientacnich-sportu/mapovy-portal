2ca86408601cbe223587ed805bb25ab71d3ba476
David Pustai <david@pustai.cz>
2019-11-13 14:33:13 +0100

    Don't send e-mails on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -62,6 +62,11 @@ Mapserver::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_method = :test
+
   config.action_mailer.default_url_options = { :host => "mapy.orientacnisporty.cz" }
 
   config.action_mailer.delivery_method = :smtp
