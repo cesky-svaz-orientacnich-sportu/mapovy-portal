7d65e461263cf4598d4aeffd82f1e6132fc724dd
David Pustai <david@pustai.cz>
2019-12-08 12:27:39 +0100

    Deploy `master` to staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,5 +2,5 @@
 set :stage, :staging
 set :deploy_to, '/home/mapserver/mapserver_staging'
 server 'goldfish.amagical.net', :app, :web, :db, :primary => true
-set :branch, "ft-replacement"
+set :branch, "master"
 set :rails_env, 'staging'
