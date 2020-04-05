4e4f3ac13d2db6798751b97f546c470a651f83e6
David Pustai <david@pustai.cz>
2020-04-05 19:10:46 +0200

    Reindent

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( _map.css _public.css _info_table.css _map.js _minimap.js _public.js mapserver/locale/cs.js  mapserver/locale/en.js )
+Rails.application.config.assets.precompile += %w( _map.css _public.css _info_table.css _map.js _minimap.js _public.js mapserver/locale/cs.js mapserver/locale/en.js )
