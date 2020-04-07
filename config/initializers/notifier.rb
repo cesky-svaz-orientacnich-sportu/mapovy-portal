51e9bf2e383bcd0885534830a86e3885976e6745
David Pustai <david@pustai.cz>
2020-04-07 11:10:43 +0200

    Configure new notification e-mail address

--- config/initializers/notifier.rb
+++ config/initializers/notifier.rb
@@ -4,6 +4,6 @@ Mapserver::Application.config.middleware.use ExceptionNotification::Rack,
   :email => {
     :email_prefix => "[Mapserver] ",
     :sender_address => %{"mapserver" <mapovyportal@orientacnisporty.cz>},
-    :exception_recipients => %w{moskyt@rozhled.cz}
+    :exception_recipients => %w{david.pustai@gmail.com}
   }
 end
