3f927834f02fd58ffa769a530b0830a7270955d3
David Pustai <david@pustai.cz>
2020-04-15 10:42:01 +0200

    Use Capistrano v3.13.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.12.1"
+lock "~> 3.13.0"
 
 set :application, "mapserver"
 set :repo_url, "git@github.com:cesky-svaz-orientacnich-sportu/mapovy-portal.git"
