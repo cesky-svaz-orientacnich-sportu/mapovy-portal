bcb853ca1f04cce735d122887a5f7188ef250d9b
David Pustai <david@pustai.cz>
2021-03-23 14:53:54 +0100

    Bump Capistrano version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+lock "~> 3.16.0"
 
 set :application, "mapserver"
 set :repo_url, "git@github.com:cesky-svaz-orientacnich-sportu/mapovy-portal.git"
