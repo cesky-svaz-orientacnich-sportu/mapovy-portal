5e4e542029fb6a9a4e7d0dfe8424fae963f291e6
David Pustai <david@pustai.cz>
2021-11-02 16:01:24 +0100

    Reindent & lint

--- app/assets/javascripts/mapserver/App/Search/Simple.js
+++ app/assets/javascripts/mapserver/App/Search/Simple.js
@@ -1,79 +1,78 @@
 App.Search.Simple = App.newClass({
 
-    constructor: function (state, resultsCallback, sidebar) {
-        App.Search.call(this, state, resultsCallback, sidebar);
-        this.init();
-    },
+	constructor: function (state, resultsCallback, sidebar) {
+		App.Search.call(this, state, resultsCallback, sidebar);
+		this.init();
+	},
 
-    Extends: App.Search,
+	Extends: App.Search,
 
-    search: function () {
-        var name = $(".toolbar-search-input").val();
-        this.searchByName(name);
-    },
+	search: function () {
+		var name = $(".toolbar-search-input").val();
+		this.searchByName(name);
+	},
 
-    init: function () {
-        this.initAutocomplete();
-        this.initKeyPressed();
-        $('.toolbar-search-submit').on('click', function(e) {
-        	e.preventDefault();
-        	searchSimple.search();
-        });
-    },
+	init: function () {
+		this.initAutocomplete();
+		this.initKeyPressed();
+		$('.toolbar-search-submit').on('click', function(e) {
+			e.preventDefault();
+			searchSimple.search();
+		});
+	},
 
-    initAutocomplete: function () {
-        where = '';
-        // visibility
-        if (! Config.user || ! (Config.user.role == 'admin' || Config.user.role == 'manager' || Config.user.role == 'cartographer')) {
-          if (Config.user) {
-            //where = Config.stateQuery + " OR created_by_id = " + Config.user.id;
-            where = Config.stateQuery;
-          } else {
-            where = Config.stateQuery;
-          }
-        }
-        console.log("Autocomplete init with query = " + where);
-        $.ajax({
-            url: '/api/select',
-            dataType: 'json',
-            data: {
-                select: ['title'],
-                where: where,
-                group_by: 'title'
-            },
-            error: function(jqxhr, status, error) {
-              console.log("Could not load autocomplete list : S = " + status + " E = " + error + " while request was sent to `/api/select`");
-            },
-            success: function (res) {
-                if (res.status == 'success') {
-                    // Create the list of results for display of autocomplete
-                    list = [];
-                    for (i = 0; i < res.data.length; i++) {
-                        list.push(res.data[i]['title']);
-                    }
+	initAutocomplete: function () {
+		where = '';
+		if (! Config.user || ! (Config.user.role == 'admin' || Config.user.role == 'manager' || Config.user.role == 'cartographer')) {
+			if (Config.user) {
+				//where = Config.stateQuery + " OR created_by_id = " + Config.user.id;
+				where = Config.stateQuery;
+			} else {
+				where = Config.stateQuery;
+			}
+		}
+		console.log("Autocomplete init with query = " + where);
+		$.ajax({
+			url: '/api/select',
+			dataType: 'json',
+			data: {
+				select: ['title'],
+				where: where,
+				group_by: 'title'
+			},
+			error: function(jqxhr, status, error) {
+				console.log("Could not load autocomplete list : S = " + status + " E = " + error + " while request was sent to `/api/select`");
+			},
+			success: function (res) {
+				if (res.status == 'success') {
+					// Create the list of results for display of autocomplete
+					list = [];
+					for (i = 0; i < res.data.length; i++) {
+						list.push(res.data[i]['title']);
+					}
 
-                    // Use the results to create the autocomplete options
-                    $(".toolbar-search-input").autocomplete({
-                        source: function(request, response) {
-                          var filtered_results = $.ui.autocomplete.filter(list, request.term);
-                          response(filtered_results.slice(0, 20));
-                        },
-                        minLength: 2
-                    });
-                } else {
-                    console.log("Could not load autocomplete list : S = error E = " + res.message + " while request was sent to `/api/select`");
-                }
-            }
-        });
-    },
+					// Use the results to create the autocomplete options
+					$(".toolbar-search-input").autocomplete({
+						source: function(request, response) {
+							var filtered_results = $.ui.autocomplete.filter(list, request.term);
+							response(filtered_results.slice(0, 20));
+						},
+						minLength: 2
+					});
+				} else {
+					console.log("Could not load autocomplete list : S = error E = " + res.message + " while request was sent to `/api/select`");
+				}
+			}
+		});
+	},
 
-    initKeyPressed: function () {
-      var this_ = this;
-      $('.toolbar-search-input').keypress(function (e) {
-          if (e.keyCode == '13') {
-              $('.ui-autocomplete').hide();
-              this_.search();
-          }
-      });
-    }
+	initKeyPressed: function () {
+		var this_ = this;
+		$('.toolbar-search-input').keypress(function (e) {
+			if (e.keyCode == '13') {
+				$('.ui-autocomplete').hide();
+				this_.search();
+			}
+		});
+	}
 });
--- app/assets/javascripts/mapserver/mapTools.js
+++ app/assets/javascripts/mapserver/mapTools.js
@@ -1,105 +1,105 @@
 function zoom2one(mapId) {
-    // zoom and center map on query results
-    $.ajax({
-        url: '/api/select',
-        dataType: 'json',
-        data: {
-            select: ['shape_kml'],
-            where: "id = " + mapId
-        },
-        error: function(jqxhr, status, error) {
-            window.alert("Could not zoom : S = " + status + " E = " + error);
-        },
-        success: function (res) {
-            if (res) {
-                alert('no response');
-            } else if (res.status == 'success') {
-                numRows = res.data.length;
-                if (numRows == 0) {
-                  window.alert("Mapa s tímto ID neexistuje");
-                }
-                if (numRows == 1) {
-                  var kml = res.data[0]['shape_kml'];
-                  var kmlString = "<Placemark>" + kml + "</Placemark>";
-                }
-                if (numRows > 1) {
-                  var count = numRows - 1;
-                  for (i = 0; i <= count; i++) {
-                    var kml = res.data[i]['shape_kml'];
-                    kmlString += "<Placemark>" + kml + "</Placemark>";
-                  }
-                }
-                if (kml == '')
-                    { window.alert("Omlouváme se, ale tato mapa nedisponuje obrysem"); }
-                // create a geoXml3 parser for the click handlers
-                var geoXml = new geoXML3.parser({
-                    map: map,
-                    zoom: false
-                });
+	$.ajax({
+		url: '/api/select',
+		dataType: 'json',
+		data: {
+			select: ['shape_kml'],
+			where: "id = " + mapId
+		},
+		error: function(jqxhr, status, error) {
+			window.alert("Could not zoom : S = " + status + " E = " + error);
+		},
+		success: function (res) {
+			if (res) {
+				alert('no response');
+			} else if (res.status == 'success') {
+				numRows = res.data.length;
+				if (numRows == 0) {
+					window.alert("Mapa s tímto ID neexistuje");
+				}
+				if (numRows == 1) {
+					var kml = res.data[0]['shape_kml'];
+					var kmlString = "<Placemark>" + kml + "</Placemark>";
+				}
+				if (numRows > 1) {
+					var count = numRows - 1;
+					for (i = 0; i <= count; i++) {
+						var kml = res.data[i]['shape_kml'];
+						kmlString += "<Placemark>" + kml + "</Placemark>";
+					}
+				}
+				if (kml == '') {
+					window.alert("Omlouváme se, ale tato mapa nedisponuje obrysem");
+				}
+				// create a geoXml3 parser for the click handlers
+				var geoXml = new geoXML3.parser({
+					map: map,
+					zoom: false
+				});
 
-                geoXml.parseKmlString(kmlString);
-                geoXml.docs[0].gpolygons[0].setMap(null);
-                google.maps.event.trigger(map, 'resize');
-                map.fitBounds(geoXml.docs[0].gpolygons[0].bounds);
-                var zoomLevel = map.getZoom();
-                map.setZoom(zoomLevel);
-            } else {
-                window.alert("Could not zoom : S = error E = " + res.message);
-            }
-        }
-    });
+				geoXml.parseKmlString(kmlString);
+				geoXml.docs[0].gpolygons[0].setMap(null);
+				google.maps.event.trigger(map, 'resize');
+				map.fitBounds(geoXml.docs[0].gpolygons[0].bounds);
+				var zoomLevel = map.getZoom();
+				map.setZoom(zoomLevel);
+			} else {
+				window.alert("Could not zoom : S = error E = " + res.message);
+			}
+		}
+	});
 }
 
 function executeAdvEnter(e) {
 	if (e.keyCode == 13) {
 		$('.ui-autocomplete').hide();
-	    searchAdvanced.search();
+		searchAdvanced.search();
 	}
 }
 
 function showSelectedItem() {
-  for(i = 0; i < queryArr.length; i++){
-	  var record = queryArr[i].split("=");
-	  var argname = record[0];
-    var value = record[1];
-    if (argname == 'club' || argname == 'year' || argname == 'author') {
-      showSelectedLayer();
-    }
-  }
+	for(i = 0; i < queryArr.length; i++){
+		var record = queryArr[i].split("=");
+		var argname = record[0];
+		var value = record[1];
+		if (argname == 'club' || argname == 'year' || argname == 'author') {
+			showSelectedLayer();
+		}
+	}
 }
 
 function geocodeAddress(address) {
-  var coords = "";
-  geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      coords = results[0].geometry.location;
-      coords = coords.lat()+","+coords.lng();
-      $('#advancedSearchGPS').val(coords);
-      $('#advancedSearchPlace').val("");
-      // searchAdvanced.clearOverlays();
-      //   var marker = new google.maps.Marker({
-      //     position: coords,
-      //     title:""
-      //     });
-      //   marker.setMap(map);
-      //
-      searchAdvanced.search();
-    } else {
-      window.alert("Geocode was not successful for the following reason: " + status);
-    }
-  });
-  return coords;
+	var coords = "";
+	geocoder.geocode( { 'address': address}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			coords = results[0].geometry.location;
+			coords = coords.lat()+","+coords.lng();
+			$('#advancedSearchGPS').val(coords);
+			$('#advancedSearchPlace').val("");
+			// searchAdvanced.clearOverlays();
+			//   var marker = new google.maps.Marker({
+			//     position: coords,
+			//     title:""
+			//     });
+			//   marker.setMap(map);
+			//
+			searchAdvanced.search();
+		} else {
+			window.alert("Geocode was not successful for the following reason: " + status);
+		}
+	});
+	return coords;
 }
 
 function showLayersFromUrl() {
-  if (urlLinkValue) {
-    $("div.item").removeClass("active");
-    $("input.fitem").attr("checked", false);
-    layers = urlLinkValue.split(",");
-    for(s = 0; s < layers.length; s++){
-      $("input#fitem" + layers[s]).attr("checked", true);
-      $("div#item" + layers[s]).addClass("item active");
-    }
-	  toc.refresh();
-  }
+	if (urlLinkValue) {
+		$("div.item").removeClass("active");
+		$("input.fitem").attr("checked", false);
+		layers = urlLinkValue.split(",");
+		for(s = 0; s < layers.length; s++){
+			$("input#fitem" + layers[s]).attr("checked", true);
+			$("div#item" + layers[s]).addClass("item active");
+		}
+		toc.refresh();
+	}
 }
