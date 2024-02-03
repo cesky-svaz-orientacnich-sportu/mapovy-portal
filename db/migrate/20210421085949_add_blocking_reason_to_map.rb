b1c78d2f6235c05adefa3a29e1a79fd27e63f4a2
David Pustai <david@pustai.cz>
2024-02-03 15:06:02 +0100

    Remove old migrations

--- db/migrate/20210421085949_add_blocking_reason_to_map.rb
+++
@@ -1,9 +0,0 @@
-class AddBlockingReasonToMap < ActiveRecord::Migration[6.1]
-  def up
-    add_column :maps, :blocking_reason, :text, { null: true }
-  end
-
-  def down
-    remove_column :maps, :blocking_reason, :text
-  end
-end
