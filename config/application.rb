1669be08541fd923199f8f84ea2bf5202c6c8126
David Pustai <david@pustai.cz>
2025-07-31 19:59:59 +1200

    Redirect unknow locales to Czech

--- config/application.rb
+++ config/application.rb
@@ -33,10 +33,9 @@ module Mapserver
     config.autoload_paths += %W(#{config.root}/app/datatables)
     config.autoload_lib(ignore: %w(assets tasks))
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :cs
+    # The default locale is :cs and available locales are specified manually.
     config.i18n.available_locales = [:cs, :en]
+    config.i18n.default_locale = :cs
     I18n.enforce_available_locales = true
 
     # Configure the default encoding used in templates for Ruby 1.9.
