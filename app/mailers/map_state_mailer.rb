41ebc0820fa65d160a259cf37eeb77e15edcd9b8
David Pustai <david.pustai@gmail.com>
2025-11-05 11:24:06 +1300

    Don't send reminders copy to admins

--- app/mailers/map_state_mailer.rb
+++ app/mailers/map_state_mailer.rb
@@ -4,35 +4,35 @@ class MapStateMailer < ActionMailer::Base
 
   def map_proposed(map)
     @map = map
-    mail(to: map.authorized_cartographers.map(&:email), cc: User.where(role: 'admin').map(&:email), subject: "Nový požadavek na evidenci mapy #{@map}")
+    mail(to: map.authorized_cartographers.map(&:email), subject: "Nový požadavek na evidenci mapy #{@map}")
   end
 
   def map_changed_after_request(map, region_was)
     @map = map
     @region_was = region_was
-    mail(to: map.authorized_cartographers(region_was).map(&:email), cc: User.where(role: 'admin').map(&:email), subject: "Doplněný požadavek na evidenci mapy #{@map}")
+    mail(to: map.authorized_cartographers(region_was).map(&:email), subject: "Doplněný požadavek na evidenci mapy #{@map}")
   end
 
   def map_approved(map)
     @map = map
-    mail(to: map.created_by.email, cc: User.where(role: 'admin').map(&:email), subject: "Požadavek na evidenci mapy #{@map} přijat")
+    mail(to: map.created_by.email, subject: "Požadavek na evidenci mapy #{@map} přijat")
   end
 
   def map_rejected(map, comment)
     @map = map
     @comment = comment
-    mail(to: map.created_by.email, cc: User.where(role: 'admin').map(&:email), subject: "Požadavek na evidenci mapy #{@map} vrácen k doplnění")
+    mail(to: map.created_by.email, subject: "Požadavek na evidenci mapy #{@map} vrácen k doplnění")
   end
 
   def map_completed(map)
     @map = map
-    mail(to: map.authorized_cartographers.map(&:email), cc: User.where(role: 'admin').map(&:email), subject: "Doplněny údaje k mapě #{@map}")
+    mail(to: map.authorized_cartographers.map(&:email), subject: "Doplněny údaje k mapě #{@map}")
   end
 
   def map_completion_rejected(map, comment)
     @map = map
     @comment = comment
-    mail(to: map.created_by.email, cc: User.where(role: 'admin').map(&:email), subject: "Požadavek na schválení mapy #{@map} vrácen k doplnění")
+    mail(to: map.created_by.email, subject: "Požadavek na schválení mapy #{@map} vrácen k doplnění")
   end
 
   def reminder_on_proposed(map)
@@ -40,7 +40,7 @@ class MapStateMailer < ActionMailer::Base
     return unless map.state == Map::STATE_PROPOSED
     @map = map
     map.update_attribute :last_reminder_sent_at, Date.today
-    mail(to: map.authorized_cartographers.map(&:email), cc: User.where(role: 'admin').map(&:email), subject: "Připomínka -- čekající požadavek na evidenci mapy #{@map}")
+    mail(to: map.authorized_cartographers.map(&:email), subject: "Připomínka -- čekající požadavek na evidenci mapy #{@map}")
   end
 
   def reminder_before_completed(map)
@@ -48,7 +48,7 @@ class MapStateMailer < ActionMailer::Base
     return unless map.state == Map::STATE_APPROVED
     @map = map
     map.update_attribute :last_reminder_sent_at, Date.today
-    mail(to: map.created_by.email, cc: User.where(role: 'admin').map(&:email), subject: "Připomínka -- je potřeba doplnit údaje k mapě #{@map} po závodě")
+    mail(to: map.created_by.email, subject: "Připomínka -- je potřeba doplnit údaje k mapě #{@map} po závodě")
   end
 
   def reminder_on_requested_change(map)
@@ -56,7 +56,7 @@ class MapStateMailer < ActionMailer::Base
     return unless map.state == Map::STATE_CHANGE_REQUESTED or map.state == Map::STATE_FINAL_CHANGE_REQUESTED
     @map = map
     map.update_attribute :last_reminder_sent_at, Date.today
-    mail(to: map.created_by.email, cc: User.where(role: 'admin').map(&:email), subject: "Připomínka -- je potřeba doplnit údaje k mapě #{@map}")
+    mail(to: map.created_by.email, subject: "Připomínka -- je potřeba doplnit údaje k mapě #{@map}")
   end
 
 end
